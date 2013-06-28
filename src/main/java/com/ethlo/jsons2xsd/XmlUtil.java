c0b9df202a21160682eb16524af100ea80706d7f
Morten Haraldsen <morten@ethlo.com>
2013-06-28 08:31:20 +0200

    Splitting and cleaning this project to allow it to be a small, single-purpose, low dependency library for conversion only

--- src/main/java/com/ethlo/jsons2xsd/XmlUtil.java
+++ src/main/java/com/ethlo/jsons2xsd/XmlUtil.java
@@ -3,11 +3,8 @@ package com.ethlo.jsons2xsd;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringWriter;
-import java.util.Collections;
-import java.util.Map;
 
 import javax.xml.XMLConstants;
-import javax.xml.namespace.NamespaceContext;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -19,16 +16,11 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
 
 import org.springframework.util.Assert;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
@@ -82,29 +74,6 @@ public class XmlUtil
 	public static Document loadDocument(Reader reader) throws SAXException, IOException
 	{
 		return getBuilder().parse(new InputSource(reader));
-	}
-
-	public static Node findNode(Node target, String namespaceUri, String localName) throws XPathExpressionException
-	{
-		final XPath xpath = getXPath(Collections.singletonMap("ns_", namespaceUri));
-		final String expr = "//ns_:" + localName;
-		return (Node) xpath.evaluate(expr, target, XPathConstants.NODE);
-	}
-	
-	public static NodeList findNodeChildren(Node target, String namespaceUri, String localName) throws XPathExpressionException
-	{
-		final XPath xpath = getXPath(Collections.singletonMap("ns_", namespaceUri));
-		final String expr = "//ns_:" + localName + "/*";
-		return (NodeList) xpath.evaluate(expr, target, XPathConstants.NODESET);
-	}
-	
-	public static XPath getXPath(Map<String, String> namespaces)
-	{
-		final XPathFactory xPathfactory = XPathFactory.newInstance();
-		final XPath xpath = xPathfactory.newXPath();
-		final NamespaceContext nsContext = new NamespaceContextMap(namespaces);
-		xpath.setNamespaceContext(nsContext);
-		return xpath;
 	}
 
 	public static Element createXsdElement(Node element, String name)
